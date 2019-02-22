e7e870e49a859752b800e846df8d6bcb3589bd90
Taraxacum <john980118@163.com>
2019-02-23 01:48:28 +0800

    cachelab trans 6.9/0.0/8.8

--- cache_lab/cachelab-handout/trans.c
+++ cache_lab/cachelab-handout/trans.c
@@ -6,7 +6,7 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -19,15 +19,32 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+void trans(int M, int N, int A[N][M], int B[M][N]);
+
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j;
+
+    for (i = 0; i < N; i += 8) {
+        for (j = 0; j < M; j += 8) {
+            int xi = i + 8, xj = j + 8;
+            int maxi = xi < N ? xi : N;
+            int maxj = xj < M ? xj : M;
+
+            for (xi = i; xi < maxi; xi++) {
+                for (xj = j; xj < maxj; xj++) {
+                    B[xj][xi] = A[xi][xj];
+                }
+            }
+        }
+    }
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
- */ 
+ */
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
@@ -42,8 +59,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -56,11 +72,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -81,4 +96,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
